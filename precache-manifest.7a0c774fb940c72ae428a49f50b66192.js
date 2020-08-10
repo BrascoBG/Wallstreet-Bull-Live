4fcc7e2f8d4da398f64faa89a30afe625de50027
Ivo Krastev <ivo.krustev@yahoo.com>
2020-08-10 13:24:41 +0300

    Updates

---
+++ precache-manifest.7a0c774fb940c72ae428a49f50b66192.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57f0383d7126746faa95afc9294512f",
+    "url": "/Wallstreet-Bull-Live/index.html"
+  },
+  {
+    "revision": "f6ba94131f2789f1101f",
+    "url": "/Wallstreet-Bull-Live/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b55f459661935109113b",
+    "url": "/Wallstreet-Bull-Live/static/css/main.a8385569.chunk.css"
+  },
+  {
+    "revision": "f6ba94131f2789f1101f",
+    "url": "/Wallstreet-Bull-Live/static/js/2.52871e34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wallstreet-Bull-Live/static/js/2.52871e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55f459661935109113b",
+    "url": "/Wallstreet-Bull-Live/static/js/main.e2057c8b.chunk.js"
+  },
+  {
+    "revision": "9a89cd290a01b81117e8",
+    "url": "/Wallstreet-Bull-Live/static/js/runtime-main.f8da1f4a.js"
+  },
+  {
+    "revision": "5f4336b37dc1236b7fcbc73b3e5a3d88",
+    "url": "/Wallstreet-Bull-Live/static/media/bg.5f4336b3.jpg"
+  },
+  {
+    "revision": "26502b6caa77a063a3437ac4f6e60594",
+    "url": "/Wallstreet-Bull-Live/static/media/bull.26502b6c.png"
+  },
+  {
+    "revision": "030c3347bf92deeef088ef75f68e7fed",
+    "url": "/Wallstreet-Bull-Live/static/media/msft.030c3347.png"
+  },
+  {
+    "revision": "6d97cf32aa6c6c83ec493586af7e80d1",
+    "url": "/Wallstreet-Bull-Live/static/media/wall.6d97cf32.jpg"
+  },
+  {
+    "revision": "8b57ad86daeddbf77e47c90f019b9e78",
+    "url": "/Wallstreet-Bull-Live/static/media/wallstreet.8b57ad86.jpg"
+  },
+  {
+    "revision": "dfb3ac235f4293442537c16781b3f034",
+    "url": "/Wallstreet-Bull-Live/static/media/wallstreet2.dfb3ac23.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Wallstreet-Bull-Live/precache-manifest.7a0c774fb940c72ae428a49f50b66192.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wallstreet-Bull-Live/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
